45fdec611ded1fa177867b3a6782d9a0ee4f91fc
Trenton <trenturd@outlook.com>
2026-09-11 03:58:18 -0700

    Delete src/main directory

--- src/main/java/cintlex/oldlighter/OldLighterAccessor.java
+++
@@ -1,8 +0,0 @@
-package cintlex.oldlighter;
-
-public interface OldLighterAccessor {
-    float oldlighter$getamblightfactor();
-    void oldlighter$setamblightfactor(float value);
-    boolean oldlighter$isirregularambient();
-    void oldlighter$setirregularambient(boolean value);
-}
